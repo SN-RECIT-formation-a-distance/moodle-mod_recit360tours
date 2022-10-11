2599fcfaf133eb4d0272b0a0acfb4cf27dd85017
Émile Jacques <emilevirus@gmail.com>
2022-10-11 11:54:16 -0400

    Update version.php

--- src/version.php
+++ src/version.php
@@ -25,9 +25,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2022100100;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2020061500.00; // Moodle 3.9.0
 $plugin->component = 'mod_recit360tours';        // Full name of the plugin (used for diagnostics)
-$plugin->dependencies = [                                                                                                        
-    'local_recitcommon' => 2021082000
-];
 $plugin->release = 'v1.0.0-alpha'; 
 $plugin->supported = [39, 400];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
 $plugin->maturity = MATURITY_ALPHA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
